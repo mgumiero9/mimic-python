6ce6bc0172cfed51a9c80065a87feb9c8513abf5
Marcelo Gusmão Gumiero <mgumiero9@gmail.com>
2020-11-01 14:17:40 -0300

    Initial commit

---
+++ mimic.py
@@ -0,0 +1,12 @@
+def combos(word):
+    obj = {}
+    arr = []
+    fixedChars = []
+    fixedPos = 0
+    stepCounter = 0
+    str = list(word)
+    objOrdinary = 0
+    step = 1
+
+
+combos("abc")
