7939f17192b5a182b9738fe00f6048da627a3f6b
Marcelo Gusmão Gumiero <mgumiero9@gmail.com>
2020-11-01 15:54:47 -0300

    compiling

--- src/mimic.py
+++ src/mimic.py
@@ -5,10 +5,12 @@ fixedPos = 0
 stepCounter = 0
 objOrdinary = 0
 step = 1
+my_str = []
 
 
 def combos(word):
-    str = list(word)
+    global my_str
+    my_str = list(word)
 
     def add_row(character):
         global objOrdinary
@@ -19,19 +21,20 @@ def combos(word):
         global step
         global stepCounter
         global fixedPos
+        global my_str
         arr = []
         if step < len(word):
             if step == 1:
-                for c in str:
+                for c in my_str:
                     add_row(c)
 
                 step += 1
             elif step == 2:
                 stepCounter += 1
-                str = list(word)
-                if fixedPos < len(str):
-                    fixedChars = str[fixedPos:step -1]
-                for c in str:
+                my_str = list(word)
+                if fixedPos < len(my_str):
+                    fixedChars = my_str[fixedPos:step -1]
+                for c in my_str:
                     arr = []
                     arr.append(fixedChars)
                     arr.append(c)
@@ -43,10 +46,10 @@ def combos(word):
                 lastObjKey = len(obj.keys()) -1
                 for key in obj.keys():
                     if len(obj[key]) == len(obj[lastObjKey]):
-                        str = list(word)
+                        my_str = list(word)
                         for c in obj[key]:
-                            str = list(filter(lambda x: x != c, str))
-                        for strChar in list(str):
+                            my_str = list(filter(lambda x: x != c, my_str))
+                        for strChar in list(my_str):
                             arr = []
                             arr.append(list(obj[key]))
                             arr.append(strChar)
